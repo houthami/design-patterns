a281641fbaae60f9b0a20b546a9e4477e45d6c5c
Hassan Outhami <hassan.outhami1@gmail.com>
2022-10-15 15:49:10 +0100

    Builder + Prototype

---
+++ src/Builder/ObjectBuilder1.java
@@ -0,0 +1,24 @@
+package Builder;
+
+public class ObjectBuilder implements Ibuilder{
+    Object object = new Object();
+    @Override
+    public void setObject1() {
+          object.setParametre1(1);
+    }
+
+    @Override
+    public void setObject2() {
+        object.setParametre2(2);
+    }
+
+    @Override
+    public void setObject3() {
+       object.setParametre3(3);
+    }
+
+    @Override
+    public Object getBuild() {
+        return object;
+    }
+}
